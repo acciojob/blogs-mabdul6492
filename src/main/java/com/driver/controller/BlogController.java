82996a7306a3abf86446ef56bdf88d4ee9eaf0c6
Abdul Rehman <mabdul6492@gmail.com>
2023-11-23 11:27:12 +0530

    all done3

--- src/main/java/com/driver/controller/BlogController.java
+++ src/main/java/com/driver/controller/BlogController.java
@@ -1,13 +1,9 @@
 package com.driver.controller;
 
-import com.driver.models.Blog;
 import com.driver.services.BlogService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/blogs")
@@ -20,7 +16,7 @@ public class BlogController {
     }
 
     @PostMapping
-    public ResponseEntity createBlog(@RequestParam Integer userId ,
+    public ResponseEntity<Void> createBlog(@RequestParam Integer userId ,
                                      @RequestParam String title,
                                      @RequestParam String content) {
         // Create a blog and add it under given user
--- src/main/java/com/driver/services/UserService.java
+++ src/main/java/com/driver/services/UserService.java
@@ -2,38 +2,33 @@ package com.driver.services;
 
 import com.driver.models.*;
 import com.driver.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserService {
 
-    UserRepository userRepository;
+    UserRepository userRepository3;
 
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserService(UserRepository userRepository3) {
+        this.userRepository3 = userRepository3;
     }
 
     public User createUser(String username, String password){
         User user = new User();
         user.setUsername(username);
         user.setPassword(password);
-        return userRepository.save(user);
+        return userRepository3.save(user);
     }
 
     public void deleteUser(int userId){
-        userRepository.deleteById(userId);
+        userRepository3.deleteById(userId);
     }
 
     public User updateUser(Integer id, String password){
-        User user = userRepository.findById(id).orElse(null);
+        User user = userRepository3.findById(id).orElse(null);
         if(user != null) {
             user.setPassword(password);
-            userRepository.save(user);
+            userRepository3.save(user);
         }
 
         return user;
